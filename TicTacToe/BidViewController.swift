6669e8c325cc49cb0fa96a4cbe568dacd3a4a269
Tanja Keune <tanjakeune@gmail.com>
2017-08-29 12:35:56 -0400

    Initial Commit

---
+++ TicTacToe/BidViewController.swift
@@ -0,0 +1,223 @@
+//
+//  BidViewController.swift
+//  TicTacToe
+//
+//  Created by Tanja Keune on 5/29/17.
+//  Copyright © 2017 Tanja Keune. All rights reserved.
+//
+
+import UIKit
+
+class BidViewController: UIViewController {
+
+    @IBOutlet weak var soreLeft: UILabel!
+    
+    @IBOutlet weak var soreRight: UILabel!
+    
+    @IBOutlet var bidTextField: UITextField!
+    
+    @IBOutlet var playerOLabel: UILabel!
+    
+    @IBOutlet var playerOPoints: UILabel!
+    
+    @IBOutlet var playerXPoints: UILabel!
+    
+    @IBOutlet var bid: UIButton!
+    
+    @IBOutlet var playerTurnImage: UIImageView!
+    
+    @IBOutlet var PlayerOImage: UIImageView!
+    @IBOutlet var PlayerXImage: UIImageView!
+    
+    var activePlayer = 1 // 1 - noughts, 2 - crosses
+    
+    var playerX = 100
+    var playerO = 100
+    
+    var scoreL = 0
+    var scoreR = 0
+    
+    var resetCredits = false
+    
+    var xBid = 0
+    var oBid = 0
+    var sum = 0
+    
+    var gameStateSaved = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    var sendPlayer = 0
+    
+    var x = 0 // 0 - didn't place a bid, 1- did placed bid
+    
+    var o = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //playerTurnLabel.text = "Player O turn. Make your bid:"
+        playerTurnImage.image = UIImage(named: "PlayerOTurn.png")
+       
+        print(gameStateSaved)
+        if resetCredits == true {
+            playerX = 100
+            playerO = 100
+        }
+        
+        playerOPoints.text = String(playerO)
+        playerXPoints.text = String(playerX)
+        
+        soreLeft.text = String(scoreL)
+        soreRight.text = String(scoreR)
+        
+        PlayerOImage.image = UIImage(named: "NoughtsPlayerLabel.png")
+        PlayerXImage.image = UIImage(named: "CrossesPlayerLabel.png")
+        //put a logo on navigation bar
+        
+        let logo = UIImage(named: "smallerBidLogo.png")
+        let imageView = UIImageView(image: logo)
+        imageView.contentMode = .scaleAspectFit
+        self.navigationItem.titleView = imageView
+        
+        //set color to navigation bar
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 79.0/255.0, green: 147.0/255.0, blue: 218.0/255.0, alpha: 1.0 )
+        
+        //set background color to view
+        
+        self.view.backgroundColor = UIColor(red: 245.0/255.0, green: 233.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+        //puts a navigation background image but has to be right size
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func bidButtonPressed(_ sender: AnyObject) {
+      
+        
+        
+        let bidCurrent = Int(bidTextField.text!)!
+ 
+        if activePlayer == 1 {
+        
+            //is noughts player
+            
+            if bidCurrent <= playerO {
+                
+                oBid = bidCurrent
+                
+                playerO -= oBid
+                
+                o = 1
+                
+               // playerTurnLabel.textColor = UIColor.red
+               // playerTurnLabel.text = "Player X turn. Make your bid:"
+                
+                playerTurnImage.image = UIImage(named: "PlayerXTurn.png")
+                bidTextField.text = ""
+                activePlayer = 2
+                
+            } else {
+                
+                //message "Not suffisant funds, you have \(playerO) pints."
+                self.alertMessageOk(title: "Don't have enugh credits.", message: "Please check you credits and bid again.")
+            }
+            
+      } else {
+            
+            //Crosses turn
+            
+            if bidCurrent <= playerX {
+                
+                xBid = bidCurrent
+                
+                playerX -= xBid
+                
+                x = 1
+                
+               // playerTurnLabel.text = "Plaer Noughts turn. Make your bid:"
+                
+                activePlayer = 1
+
+                // check who won
+                //
+                if o == 1 && x == 1 {
+                    
+                    o = 0
+                    x = 0
+                    
+                    if xBid > oBid {
+                        
+                        playerTurnImage.image = UIImage(named: "PlayerXTurn.png")
+                        bidTextField.isHidden = true
+                        playerO += xBid + oBid
+                        //won call segue to the board
+                        
+                        sendPlayer = 2
+                        print(sendPlayer)
+                        performSegue(withIdentifier: "play", sender: sendPlayer)  //2
+                        
+                    } else if xBid == oBid {
+                        
+                        //add alert to tell them they had equal bids so they should go again
+                        
+                        playerX += xBid
+                        playerO += oBid
+                        playerTurnImage.image = UIImage(named: "PlayerOTurn.png")
+                        bidTextField.text = ""
+                        
+                    } else {
+                        
+                        playerTurnImage.image = UIImage(named: "checkPoints.png")
+                        bidTextField.isHidden = true
+                        playerX += xBid + oBid
+                        //won call segue to the board
+                        
+                        sendPlayer = 1
+                        print(sendPlayer)
+                        performSegue(withIdentifier: "play", sender: sendPlayer)  //1
+                        
+                    }
+                    playerXPoints.text = String(playerX)
+                    playerOPoints.text = String(playerO)
+                    bidTextField.isHidden = false
+
+                }
+            }
+            
+            else {
+                
+                    self.alertMessageOk(title: "Don't have enugh credits for the bid", message: "Please check you credits and place new bid")
+
+            }
+            
+        
+    
+    
+    }
+    /*    } else {
+            //call alert to tell them to enter number
+            
+            self.alertMessageOk(title: "Please input you bid", message: "Ready to continue?")
+        }
+    */
+}
+    
+override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "play" {
+            
+            let destination = segue.destination as! ViewController
+                
+            destination.player = sendPlayer
+            destination.gameState = gameStateSaved
+            destination.playerOPoints = playerO
+            destination.playerXPoints = playerX
+            destination.complitedTurn = false
+            destination.scoreR = scoreR
+            destination.scoreL = scoreL
+            
+        }
+    }
+}
---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,292 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Tanja Keune on 5/29/17.
+//  Copyright © 2017 Tanja Keune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var winningImage: UIImageView!
+    
+    @IBOutlet var playAgainButton: UIButton!
+    
+    @IBOutlet var button: UIButton!
+    
+    //1 is noughts, 2 is cross
+    
+    var player = 0 // 0 - no player 1 - noughts, 2 - crosses
+    
+    var playerOPoints = Int()
+    
+    var playerXPoints = Int()
+    
+    var scoreL = 0
+    var scoreR = 0
+    
+    var resetCredits = false
+    
+    @IBOutlet var logoView: UIImageView!
+    
+    var activeGame = true
+    
+    var complitedTurn = false
+    
+    
+    
+    var winningCombitnation = [[2, 4, 6], [0,4,8], [0, 1, 2], [0,3,6], [1, 4, 7], [2, 5 , 8], [3, 4, 5], [6, 7, 8]]
+    
+    var gameState = [Int](repeatElement(0, count: 9))  // 0 - empty, 1 -noughts, 2 - crosses
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+//        Present WinningImage with players turn
+        
+        if player == 1 {
+            winningImage.alpha = 1
+            winningImage.image = UIImage(named: "OTurn.png")
+            UIView.animate(withDuration: 0.5) {
+                
+                self.winningImage.center = CGPoint(x: self.winningImage.center.x + 500, y: self.winningImage.center.y)
+            }
+            
+//            (winningImageTakeAWay())
+            self.perform(#selector(winningImageTakeAWay), with: nil, afterDelay: 1.0)
+            
+        } else {
+            winningImage.alpha = 1
+            winningImage.image = UIImage(named: "XTurn.png")
+            UIView.animate(withDuration: 0.5) {
+                
+                self.winningImage.center = CGPoint(x: self.winningImage.center.x + 500, y: self.winningImage.center.y)
+            }
+            
+//            winningImageTakeAWay()
+            self.perform(#selector(winningImageTakeAWay), with: nil, afterDelay: 1.0)
+
+        }
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        set button size
+        let screenWidth = UIScreen.main.bounds.width
+        
+        winningImage.alpha = 0
+        winningImage.center = CGPoint(x: winningImage.center.x - 500, y: winningImage.center.y)
+        
+        
+        winningImage.frame.size = CGSize(width: screenWidth * 4 / 3, height: screenWidth * 4 / 3)
+        
+        let buttonSize = (screenWidth - 30) / 3
+        
+        for i in 1 ... 9 {
+            
+            button = self.view.viewWithTag(i) as! UIButton
+            
+            button.frame.size = CGSize(width: buttonSize, height: buttonSize)
+            
+        }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        playAgainButton.isHidden = true
+        
+        //hides the navigation bar
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        
+        //sets background color on view controller
+        self.view.backgroundColor = UIColor(red: 245.0/255.0, green: 233.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+        
+        // After reopening the view present the played moves
+//        if player == 2 {
+//            labelPlayersTurn.text = "Crosses turn."
+//        } else {
+//            labelPlayersTurn.text = "Noughts turn."
+//        }
+        
+        for i in 1...9 {
+            
+            //let tmpButton = self.view.viewWithTag(i+1) as? UIButton
+            
+            button = self.view.viewWithTag(i) as! UIButton!
+            if gameState[i-1] == 1 {
+                
+                button.setImage(UIImage(named: "noughts.png"), for: [])
+            } else if gameState[i-1] == 2 {
+                button.setImage(UIImage(named: "crosses.png"), for: [])
+            }
+        }
+        
+        logoView.image = UIImage(named: "smallerBidLogo.png")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func ButtonPressed(_ sender: AnyObject) {
+        
+        if !complitedTurn {
+
+        let activePosition = sender.tag - 1
+        
+        if gameState[activePosition] == 0 && activeGame {
+            
+            if player == 1 {
+                
+                sender.setImage(UIImage(named: "noughts.png"), for: [])
+                
+                player = 2
+                
+                gameState[activePosition] = 1
+                
+            } else {
+                
+                sender.setImage(UIImage(named: "crosses.png"), for: [])
+                
+                player = 1
+                
+                gameState[activePosition] = 2
+            }
+        }
+        
+        for combination in winningCombitnation {
+            
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                
+                // we have a winner
+                activeGame = false
+                playAgainButton.isHidden = false
+                
+                switch gameState[combination[0]] {
+                case 1:
+                    winningImage.alpha = 1
+                    winningImage.image = UIImage(named: "GroupO.png")
+                    
+                    scoreL += 1
+                    
+                    UIView.animate(withDuration: 0.5) {
+                        
+                        self.winningImage.center = CGPoint(x: self.winningImage.center.x + 500, y: self.winningImage.center.y)
+                    }
+                    
+                case 2:
+                    
+                    winningImage.alpha = 1
+                   
+                    winningImage.image = UIImage(named: "GroupX.png")
+
+                    scoreR += 1
+                    
+                    UIView.animate(withDuration: 0.5) {
+                        
+                        self.winningImage.center = CGPoint(x: self.winningImage.center.x + 500, y: self.winningImage.center.y)
+                    }
+
+                default:
+                    return
+                }
+                
+                //clean the board and
+                resetCredits = true
+                gameState = [Int](repeatElement(0, count: 9))
+                
+            } else {
+                var tie = true
+                for state in gameState {
+                    if state == 0 {
+                        tie = false
+                    }
+                }
+                if tie == true {
+                    self.activeGame = false
+                    //self.winLabel.isHidden = false
+                    self.alertMessageOk(title: "This game is tied", message: "Try again")
+                    //self.winLabel.text = "This game is tied!"
+                    self.playAgainButton.isHidden = false
+                }
+            }
+        }
+        complitedTurn = true
+            
+        }
+        
+        if complitedTurn && activeGame {
+            
+            self.perform(#selector(callSegue), with: nil, afterDelay: 2.0)
+
+        }
+    }
+    
+    func callSegue() {
+        
+        performSegue(withIdentifier: "goBid", sender: nil)
+    }
+    
+    @IBAction func playAgain(_ sender: AnyObject) {
+        
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        
+        activeGame = true
+        
+        player = 1
+        
+        for i in 1 ..< 10 {
+            
+            if let button = view.viewWithTag(i) as? UIButton {
+                
+                button.setImage(nil, for: [])
+            }
+            
+        }
+        playAgainButton.isHidden = true
+        performSegue(withIdentifier: "goBid", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        if segue.identifier == "goBid" {
+            
+            if let navVC = segue.destination as? UINavigationController {
+                
+                if let destination = navVC.topViewController as? BidViewController {
+                    
+                    destination.gameStateSaved = gameState
+                    destination.playerX = playerXPoints
+                    destination.playerO = playerOPoints
+                    destination.resetCredits = resetCredits
+                    destination.scoreL = scoreL
+                    destination.scoreR = scoreR
+                    
+                    
+                }
+            }
+        }
+        
+        
+    }
+    
+// winning image disapear
+    
+    func winningImageTakeAWay() {
+        
+        UIView.animate(withDuration: 0.5) {
+         
+            self.winningImage.center = CGPoint(x: self.winningImage.center.x - 500, y: self.winningImage.center.y)
+            
+        }
+        winningImage.alpha = 0
+    }
+    
+    
+}
+
